921925e8049b4f50d716e801c6e3e39b71b71aa0
rudy0609 <aqillashevanaya@gmail.com>
2024-11-23 03:04:51 +0700

    Initialize warengonzaga/main

---
+++ src/consts/parameters.ts
@@ -0,0 +1,30 @@
+/** Change these values to configure the application for your own use. **/
+
+// Your smart contract address (available on the thirdweb dashboard)
+// For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
+export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+
+// The name of the chain your contract is deployed to.
+// For standard chains. (e.g. "mainnet", "rinkeby", "polygon", "mumbai", "base")
+// For custom chains, provide the chain name from thirdweb chain list or use the import name from @thirdweb-dev/chains.
+// Read: https://portal.thirdweb.com/react/react.thirdwebprovider#activechain-recommended
+// Chainlist: https://thirdweb.com/chains
+// e.g."{Sepolia}" or "{Gnosis}"
+export const chainConst = "mumbai";
+
+// It is IMPORTANT to provide your own API key to use the thirdweb SDK and infrastructure.
+// Please ensure that you define the correct domain for your API key from the API settings page.
+// You can get one for free at https://thirdweb.com/create-api-key
+// Learn more here: https://blog.thirdweb.com/changelog/api-keys-to-access-thirdweb-infra
+export const clientIdConst = "";
+
+// Configure the primary color for buttons and other UI elements
+export const primaryColorConst = "blue";
+
+// Choose between "light" and "dark" mode
+export const themeConst = "dark";
+
+// Gasless relayer configuration options
+export const relayerUrlConst = ""; // OpenZeppelin relayer URL
+export const biconomyApiKeyConst = ""; // Biconomy API key
+export const biconomyApiIdConst = ""; // Biconomy API ID
